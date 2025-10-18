3c9188b3a393b3f423b930f7a3b8850c7fe7c39b
Simone Logatto <simone.logatto@icloud.com>
2025-10-18 18:53:43 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// sostituisci "tuo-username" e "nome-repo" con i tuoi dati
 export default defineConfig({
   plugins: [react()],
   base: '/tolkien-books-app/', 
